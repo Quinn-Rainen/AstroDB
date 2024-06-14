902ce6469347ad363bc2c4fb17d259ae4c88a0b8
Quinn Rainen <41759774+Quinn-Rainen@users.noreply.github.com>
2024-06-13 22:08:07 -0700

    Bug Fixed?

--- search_magnitude.php
+++ search_magnitude.php
@@ -15,7 +15,6 @@ or die('Error connecting to MySQL server.');
 <body bgcolor="white">
   <hr>
   <?php
-  if(isset($_POST['min_magnitude']) && isset($_POST['max_magnitude'])) {
       $min_magnitude = $_POST['min_magnitude'];
       $max_magnitude = $_POST['max_magnitude'];
 
@@ -27,32 +26,29 @@ or die('Error connecting to MySQL server.');
                 FROM CelestialObjects
                 WHERE Magnitude BETWEEN $min_magnitude AND $max_magnitude";
 
-      echo "<p>The query:</p>";
-      echo "<pre>$query</pre>";
+        <p>
+        The query:
+        <p>
+        <?php
+        print $query;
+        ?>
+
 
       $result = mysqli_query($conn, $query)
       or die(mysqli_error($conn));
 
-      // Check number of rows returned
-      $num_rows = mysqli_num_rows($result);
-      echo "<p>Number of results: $num_rows</p>";
 
-      if ($num_rows > 0) {
-          echo "<p><strong>Name</strong> | <strong>Type</strong> | <strong>Magnitude</strong></p><hr>";
+    echo "<p><strong>Name</strong> | <strong>Type</strong> | <strong>Magnitude</strong></p><hr>";
 
-          while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-          {
-              printf("%s | %s | %.2f<br><br>",
-                     $row['Name'], $row['Type'], $row['Magnitude']);
-          }
-      } else {
-          echo "<p>No results found for the given magnitude range.</p>";
-      }
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    {
+        printf("%s | %s | %.2f<br><br>",
+                $row['Name'], $row['Type'], $row['Magnitude']);
+    }
 
-      mysqli_free_result($result);
-  } else {
-      echo "<p>Magnitude values are not set.</p>";
-  }
+
+    mysqli_free_result($result);
+
 
   mysqli_close($conn);
   ?>
