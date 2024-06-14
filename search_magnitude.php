6d65207c5e83601b80a5177f794c058416893ffb
Quinn Rainen <41759774+Quinn-Rainen@users.noreply.github.com>
2024-06-13 22:05:01 -0700

    More bugs

--- search_magnitude.php
+++ search_magnitude.php
@@ -15,39 +15,45 @@ or die('Error connecting to MySQL server.');
 <body bgcolor="white">
   <hr>
   <?php
-  $min_magnitude = $_POST['min_magnitude'];
-  $max_magnitude = $_POST['max_magnitude'];
+  if(isset($_POST['min_magnitude']) && isset($_POST['max_magnitude'])) {
+      $min_magnitude = $_POST['min_magnitude'];
+      $max_magnitude = $_POST['max_magnitude'];
 
-  $min_magnitude = mysqli_real_escape_string($conn, $min_magnitude);
-  $max_magnitude = mysqli_real_escape_string($conn, $max_magnitude);
+      $min_magnitude = mysqli_real_escape_string($conn, $min_magnitude);
+      $max_magnitude = mysqli_real_escape_string($conn, $max_magnitude);
 
-  // select celestial objects within a specified magnitude range by the user
-  $query = "SELECT Name, Type, Coordinates, Magnitude, ImagePath
-            FROM CelestialObjects
-            WHERE Magnitude BETWEEN $min_magnitude AND $max_magnitude";
+      // select celestial objects within a specified magnitude range by the user
+      $query = "SELECT Name, Type, Coordinates, Magnitude, ImagePath
+                FROM CelestialObjects
+                WHERE Magnitude BETWEEN $min_magnitude AND $max_magnitude";
 
-  ?>
+      echo "<p>The query:</p>";
+      echo "<pre>$query</pre>";
 
-  <p>The query:</p>
-  <?php
-  print $query;
-  ?>
+      $result = mysqli_query($conn, $query)
+      or die(mysqli_error($conn));
 
-  <hr>
-  <p>Result of query:</p>
+      // Check number of rows returned
+      $num_rows = mysqli_num_rows($result);
+      echo "<p>Number of results: $num_rows</p>";
 
-  <?php
-  $result = mysqli_query($conn, $query)
-  or die(mysqli_error($conn));
+      if ($num_rows > 0) {
+          echo "<p><strong>Name</strong> | <strong>Type</strong> | <strong>Magnitude</strong></p><hr>";
 
-  echo "<p><strong>Name</strong> | <strong>Type</strong> | <strong>Magnitude</strong></p><hr>";
+          while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+          {
+              printf("%s | %s | %.2f<br><br>",
+                     $row['Name'], $row['Type'], $row['Magnitude']);
+          }
+      } else {
+          echo "<p>No results found for the given magnitude range.</p>";
+      }
 
-    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-    {
-        printf("%s | %s | %.2f<br><br>",
-                $row['Name'], $row['Type'], $row['Magnitude']);
-    }
-  mysqli_free_result($result);
+      mysqli_free_result($result);
+  } else {
+      echo "<p>Magnitude values are not set.</p>";
+  }
+
   mysqli_close($conn);
   ?>
 
